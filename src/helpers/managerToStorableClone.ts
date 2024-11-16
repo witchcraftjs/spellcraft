9de3831001f884bdab2b05d2c0fe20d25629da77
Alan North <alanscodelog@gmail.com>
2024-11-15 21:05:17 -0300

    v0feat: added managerToStorableClone helper

---
+++ src/helpers/managerToStorableClone.ts
@@ -0,0 +1,26 @@
+import type { DeepPartial } from "@alanscodelog/utils/types"
+import { walk } from "@alanscodelog/utils/walk.js"
+
+import { type Manager } from "../types/index.js"
+
+/**
+ * Strips the manager of properties that should not be saved such as `hooks`, `listener`, `state`, and the following function options:
+ *
+ * - `evaluateCondition`
+ * - `cb`
+ * - `sorter`
+ * - `stringifier`
+ * - `conditionEquals`
+ */
+export function managerToStorableClone(m: Manager): DeepPartial<Manager> {
+	const clone = walk(m, undefined, { save: true })
+	delete clone.hooks
+	delete clone.listener
+	delete clone.state
+	delete clone.options.evaluateCondition
+	delete clone.options.cb
+	delete clone.options.sorter
+	delete clone.options.stringifier
+	delete clone.options.conditionEquals
+	return clone
+}
